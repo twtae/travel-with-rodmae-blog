88bcb87b6b4a77e3142eaaf23f182f0144b18dd0
twtae <taewich.01@gmail.com>
2024-06-08 16:48:31 +0700

    edit swup.js

--- js/swup.js
+++ js/swup.js
@@ -1,7 +1,5 @@
 import Swup from 'https://unpkg.com/swup@4?module';
-import SwupParallelPlugin from 'https://unpkg.com/@swup/parallel-plugin@0?module';
 
 const swup = new Swup({
   containers: ["#swup"],
-  plugins: [new SwupParallelPlugin()]
 });
