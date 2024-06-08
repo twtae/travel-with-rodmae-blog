93c70ee40ea7bc5da8131642b7bb83debc591294
twtae <taewich.01@gmail.com>
2024-06-08 13:50:18 +0700

    add stop resize transition function to main.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+(function () {
+    const classes = document.body.classList;
+    let timer = 0;
+    window.addEventListener('resize', function () {
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        }
+        else
+            classes.add('stop-transitions');
+
+        timer = setTimeout(() => {
+            classes.remove('stop-transitions');
+            timer = null;
+        }, 100);
+    });
+})();
